879f0d1b217d4adc2e56ffa04e5d0b2290e78bb1
MSLC <MSLC@MSLC.ad>
2017-07-05 17:25:52 +0530

    Integrated todo;

---
+++ Demo_05_MockApi/src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+	authors: 
+	[
+		{
+			id: 'cory', 
+			firstName: 'Peter', 
+			lastName: 'House'
+		},	
+		{
+			id: 'scott-allen', 
+			firstName: 'Scott', 
+			lastName: 'Allen'
+		},	
+		{
+			id: 'dan-wahlin', 
+			firstName: 'Dan', 
+			lastName: 'Wahlin'
+		}
+	]
+};
