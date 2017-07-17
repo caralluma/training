f3185177d19e17b328d3af01637888ff6832cd08
Prabhu Sunil <PS00373572@TechMahindra.com>
2017-07-17 17:00:56 +0530

    added ebooks

---
+++ xtras/setpath/setpath.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+set path=%path%;C:\Users\mslc\AppData\Roaming\npm;
+set path=%path%;C:\work\win-bash;
+set path=%path%;C:\work\mongdb\bin;
+set path=%path%;C:\Python27
+rem set HTTP_PROXY=http://ps00373572:TechM1114%40sp@10.30.0.155:80
+rem set HTTPS_PROXY=http://"ps00373572:TechM1114%40sp"@10.30.0.155:80
+rem set HTTP_PROXY=http://"ps00373572:TechM1114%40sp"@10.53.130.55:8080
+rem set HTTPS_PROXY=http://"ps00373572:TechM1114%40sp"@10.53.130.55:8080
